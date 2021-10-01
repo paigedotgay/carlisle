9cfa40ec07e694453cf47a9ededd1cc8df1cbff3
paige <qanazoga@gmail.com>
2021-10-01 06:10:46 +0000

    remove warframe-menu-listener (snuck in)

--- src/carlisle/core.clj
+++ src/carlisle/core.clj
@@ -70,7 +70,6 @@
                                   CacheFlag/ROLE_TAGS
                                   CacheFlag/VOICE_STATE])
                     (setMemberCachePolicy MemberCachePolicy/ALL)
-                    (addEventListeners (into-array Object [message-listener 
-                                                           warframe-command-menu-listener]))
+                    (addEventListeners (into-array Object [message-listener]))
                   (build))))
 
