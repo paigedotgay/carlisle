bc5aeed531e78f7dae4cb2ae250ddbbd586a3732
paige <qanazoga@gmail.com>
2021-09-30 23:48:38 -0600

    add info command, failure message on slages

--- src/carlisle/core.clj
+++ src/carlisle/core.clj
@@ -4,13 +4,16 @@
         [carlisle.logging :only [log-message]]
         [carlisle.ask.command]
         [carlisle.dead-by-daylight.command]
+        [carlisle.info.command]
         [carlisle.mtg.command]
         [carlisle.repl.command]
+        [carlisle.utils]
         [carlisle.warframe.command]
         [clojure.java.javadoc])
   (:import [net.dv8tion.jda.api JDABuilder Permission]
            [net.dv8tion.jda.api.events.message MessageReceivedEvent]
            [net.dv8tion.jda.api.hooks ListenerAdapter]
+           [net.dv8tion.jda.api.interactions.components Button]
            [net.dv8tion.jda.api.requests GatewayIntent]
            [net.dv8tion.jda.api.utils.cache CacheFlag]
            [net.dv8tion.jda.api.utils MemberCachePolicy]
@@ -33,11 +36,23 @@
           (eval-command event)))
       
       (onSlashCommand [event]
-        (case (.getName event)
-          "ask"              (ask-command event)
-          "warframe"         (warframe-command event)
-          "dead-by-daylight" (dead-by-daylight-command event)
-          "mtg"              (mtg-command event)))))
+        (try
+          (case (.getName event)
+            "ask"              (ask-command event)
+            "dead-by-daylight" (dead-by-daylight-command event)
+            "info"             (info-command event)
+            "mtg"              (mtg-command event)
+            "warframe"         (warframe-command event))
+          
+          (catch Exception e  
+            (.. event
+                (reply (format "Something went wrong!%nClick one of the links below, describe what you were trying to do, and provide this error code: `%s`"
+                               (.getMessage e)))
+                (addActionRow #{(Button/link (protocol-link :support-server) "Join the support server")
+                                (Button/link (str (config :repo) "/issues/new") "Leave an issue on GitLab")
+                                (Button/link (protocol-link :owner) "DM the owner (for friends only)")})
+                (setEphemeral true)
+                (queue)))))))
   
   (def carlisle (.. (JDABuilder/create (config :token) 
                                        [GatewayIntent/GUILD_MEMBERS 
@@ -57,5 +72,5 @@
                     (setMemberCachePolicy MemberCachePolicy/ALL)
                     (addEventListeners (into-array Object [message-listener 
                                                            warframe-command-menu-listener]))
-                    (build))))
+                  (build))))
 
--- src/carlisle/info/command.clj
+++ src/carlisle/info/command.clj
@@ -1,12 +1,14 @@
 (ns carlisle.info.command
   (:gen-class)
   (:use [carlisle.config :only [config]]
-        [carlisle.utils])
+        [carlisle.utils]
+        [clojure.java.shell :only [sh]])
+  (:require
+        [clojure.string :as str])
   (:import [net.dv8tion.jda.api EmbedBuilder]
-           [net.dv8tion.jda.api.events.interaction SelectionMenuEvent]
            [net.dv8tion.jda.api.interactions.commands OptionType Command]
            [net.dv8tion.jda.api.interactions.commands.build CommandData OptionData]
-           [net.dv8tion.jda.api.interactions.components.selections SelectionMenu]))
+           [net.dv8tion.jda.api.interactions.components Button]))
 
 (defn get-millis []
   (.getUptime (java.lang.management.ManagementFactory/getRuntimeMXBean)))
@@ -19,32 +21,44 @@
     (filterv #(not (str/blank? %)))
     (reverse)
     (rest)
-    (zipmap [:seconds :minutes :hours :days :months])))
+    (zipmap [:seconds :minutes :hours :days :months :years])))
 
-(defn build-info-embed
-  [event]
-  (let [bot (.. event getJDA)]
+(defn build-info-embed [event]
+  (let [bot (.. event getJDA)
+        ping (.. bot getRestPing complete)]
     (-> (build-basic-embed event)
-        (.addField "Support Server" (config :server-invite))
-        (.addField "Source Code" (config :repo))
-        (.addField (str "Invite " (.. bot
-                                      (getSelfUser)
-                                      (getAsMention)))
-                   (config :bot-invite))
-        (.addField "Guilds:" (count (.. bot getGuilds)) true)
-        (.addField "Users:" (count (.. bot getUsers)) true)
-        (.addField "Ping:" (.. bot getRestPing) true)
-        (.addField "Bot Uptime:" ))))
+        (.setThumbnail (.. bot getSelfUser getAvatarUrl))
+        (.addField "Guilds:" (str (count (.. bot getGuilds))) true)
+        (.addField "Users:" (str (count (.. bot getUsers))) true)
+        (.addField "Ping:" (format "%dms" ping) true)
+        (.addField "Bot Uptime:" 
+                   (let [largest (last (start-formatting (get-millis)))]
+                     (str (val largest) \space (name (key largest))))
+                   true)
+        (.addField "Server Uptime" 
+                   (-> (sh "uptime" "-p")
+                       (:out)
+                       (str/split #"( |,|\n)")
+                       (rest)
+                       (#(str (first  %) \space (second %))))
+                   true)
+        .build)))
 
 (def info-command-data
-  (.. (CommandData. "info" "Get useful bot data")))
+  (.. (CommandData. "info" "Get useful bot data")
+      (addOption OptionType/BOOLEAN
+                 "show-everyone"
+                 "would you like everyone to be able to see the result?")))
       
 (defn info-command 
   [event]
-  (let [perks (case (.. event (getOption  "role") (getAsString))
-                "survivor" sperks
-                "killer" kperks)]
+  (let [ephemeral? (if-not (empty? (.. event (getOptionsByName "show-everyone")))
+                     (not (.. event (getOption "show-everyone") getAsBoolean))
+                     true)]
     (.. event
-        (replyEmbeds (build-perk-roulette-embeds perks event))
-        (setEphemeral true)
+        (replyEmbeds #{(build-info-embed event)})
+        (addActionRow #{(Button/link (protocol-link :support-server) "Join the Support Server")
+                        (Button/link (config :repo) "View the Source Code")})
+        (addActionRow #{(Button/link (config :bot-invite) "Invite Carlisle to Your Server")})
+        (setEphemeral ephemeral?)
         (queue))))
